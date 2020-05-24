de378cc88c1550a89edab8133caa15fe84e76bc5
Akram <m.akram.arar@gmail.com>
2020-05-24 11:02:15 +0200

    Constructeur

--- src/main/java/PGL/pglp_5_2/PersonnelJDBC.java
+++ src/main/java/PGL/pglp_5_2/PersonnelJDBC.java
@@ -1,9 +1,11 @@
 package PGL.pglp_5_2;
 
+import java.beans.Statement;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 /**
  * 
@@ -11,7 +13,34 @@ import java.sql.SQLException;
  *
  */
 public class PersonnelJDBC extends DAO<Personnel>{
-
+	/**
+	 * connection
+	 */
+	 private Connection connexion = null;
+	  /**
+	   *  statement.
+	   */
+	  private Statement statement;
+	/**
+	 * Constructor
+	 */
+	public PersonnelJDBC() {
+		  connexion = this.getConnection();
+		    try {
+		      ResultSet res = connexion.getMetaData().getTables(null,null,"Personnels".toUpperCase(),null);
+		      statement = (Statement) connexion.createStatement();
+		      if (!res.next()) {
+		    	  ((java.sql.Statement)statement).execute("create table PERSONNEL(nom varchar(50)"
+		        					+ "NOT NULL PRIMARY KEY, " 
+		        					+ " prenom  varchar(50) NOT NULL, fonction varchar(50) ,"
+		        					+ "id_groupe integer));");
+		      }
+		      ((Connection) statement).close();
+		      connexion.close();
+		    } catch (SQLException e1) {
+		      e1.printStackTrace();
+		    }
+	}
 	@Override
 	public Personnel create(Personnel obj) throws FileNotFoundException, IOException {
 		// TODO Auto-generated method stub
@@ -36,7 +65,6 @@ public class PersonnelJDBC extends DAO<Personnel>{
 		return null;
 	}
 	public Connection getConnection() {
-	    Connection connexion = null;
 	    String driver = "org.apache.derby.jdbc.EmbeddedDriver";
 	    try {
 	      Class.forName(driver);
