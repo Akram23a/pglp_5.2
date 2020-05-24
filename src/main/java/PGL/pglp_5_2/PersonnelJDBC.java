727a9d8c20b0a0f64c5cff74ea4bb2d26a5fa5dd
Akram <m.akram.arar@gmail.com>
2020-05-24 10:53:09 +0200

    JDBC start

---
+++ src/main/java/PGL/pglp_5_2/PersonnelJDBC.java
@@ -0,0 +1,32 @@
+package PGL.pglp_5_2;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class PersonnelJDBC extends DAO<Personnel>{
+
+	@Override
+	public Personnel create(Personnel obj) throws FileNotFoundException, IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Personnel obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Personnel update(Personnel obj) throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Personnel find(int id) throws IOException, ClassNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
