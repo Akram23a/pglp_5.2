0db8b08669a7c91e5513dd85672791cb4a5ab800
Akram <m.akram.arar@gmail.com>
2020-05-24 10:54:37 +0200

    getConnection

--- src/main/java/PGL/pglp_5_2/PersonnelJDBC.java
+++ src/main/java/PGL/pglp_5_2/PersonnelJDBC.java
@@ -2,7 +2,14 @@ package PGL.pglp_5_2;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/**
+ * 
+ * @author Akram
+ *
+ */
 public class PersonnelJDBC extends DAO<Personnel>{
 
 	@Override
@@ -28,5 +35,19 @@ public class PersonnelJDBC extends DAO<Personnel>{
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	public Connection getConnection() {
+	    Connection connexion = null;
+	    String driver = "org.apache.derby.jdbc.EmbeddedDriver";
+	    try {
+	      Class.forName(driver);
+	    } catch (ClassNotFoundException e) {
+	      e.printStackTrace();
+	    }
+	    try {
+	      connexion = DriverManager.getConnection("jdbc:derby:sarradb;create=true");
+	    } catch (SQLException e) {
+	      e.printStackTrace();
+	    }
+	    return connexion;
+	  }
 }
